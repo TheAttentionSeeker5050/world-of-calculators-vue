24d869bb5826c3ca3b1475c37d294da991ab0389
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-05-22 17:01:32 -0300

    added form data model properties

--- src/views/Calculators/MortgagePayoffCalculator.vue
+++ src/views/Calculators/MortgagePayoffCalculator.vue
@@ -1,5 +1,21 @@
 <script >
     export default {
+        data() {
+            return {
+                // form variables
+                originalLoanAmount: 400000,
+                originalLoanTerm: 30,
+                anualInterestRate: 6,
+                remainingTermYears: 25,
+                remainingTermMonths: 0,
+
+                // return variables
+
+                // list objects
+
+                // chart variables
+            };
+        },
         mounted() {
             document.title = "World of Calculators - Mortgage Payoff Calculator";
         }
@@ -12,20 +28,41 @@
         <form id="calculator-form" class="container-fluid ">
             <div class="row">
                 <div class="input-group mb-3 col-md">
-                    <span class="input-group-text" id="og-loan-amount">Original loan amount</span>
-                    <input type="number" class="form-control" aria-describedby="og-loan-amount">
+                    <span class="input-group-text" id="og-loan-amount">Original loan amount:</span>
+                    <input type="number" class="form-control" aria-describedby="og-loan-amount" v-model="originalLoanAmount">
+                    <span class="input-group-text" >$</span>
+
                 </div>
                 <div class="input-group mb-3 col-md">
-                    <span class="input-group-text" id="og-loan-term">Original loan term</span>
-                    <input type="number" class="form-control" aria-describedby="og-loan-term">
+                    <span class="input-group-text" id="og-loan-term">Original loan term:</span>
+                    <input type="number" class="form-control" aria-describedby="og-loan-term" v-model="originalLoanTerm">
+                    <span class="input-group-text" >Years</span>
+
                 </div>
             </div>
+            
+            
             <div class="row">
                 <div class="input-group mb-3 col-md">
-                    <span class="input-group-text" id="remaining-loan-term">Remaining loan term</span>
-                    <input type="number" class="form-control" placeholder="years" aria-describedby="remaining-loan-term">
-                    <input type="number" class="form-control" placeholder="months" aria-describedby="remaining-loan-term">    
+                    <span class="input-group-text" id="remaining-loan-term">Remaining loan term:</span>
+                    <input type="number" class="form-control" placeholder="years" aria-describedby="remaining-loan-term" v-model="remainingTermYears">
+                    <span class="input-group-text" >Years</span>
+                    <input type="number" class="form-control" placeholder="months" aria-describedby="remaining-loan-term__years" v-model="remainingTermMonths">    
+                    <span class="input-group-text" >Months</span>
+
                 </div>
+                <!-- <div class="input-group mb-3 col-md"></div> -->
+
+            </div>
+
+            <div class="row">
+                <div class="input-group mb-3 col-md">
+                    <span class="input-group-text" id="anual-interest-rate">InterestRate:</span>
+                    <input type="number" class="form-control" placeholder="%" aria-describedby="anual-interest-rate" v-model="anualInterestRate">
+                    <span class="input-group-text" >%</span>
+                    
+                </div>
+                <div class="input-group mb-3 col-md"></div>
             </div>
 
             <h4 class="my-3">Repayment Options</h4>
