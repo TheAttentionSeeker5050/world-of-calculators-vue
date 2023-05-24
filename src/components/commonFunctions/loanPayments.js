02fe9a5e908c03b07d47a359cec382856a514201
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-05-23 22:49:13 -0300

    need to add summary section using str format

--- src/components/commonFunctions/loanPayments.js
+++ src/components/commonFunctions/loanPayments.js
@@ -1,3 +1,5 @@
+import { end } from "@popperjs/core";
+
 export function calcLoanRecurrentPayment(loanAmount, interestRate,numberOfPayments) {
     // this function returns a generic loan recurrent payment
     // the interest rate must be as a fraction and be the the rate effective on each payment, like monthly rate, or bi-weekly, but remember that the  term unit must match the number of payments as well
@@ -55,6 +57,74 @@ export function genericAmortizationTable(interestRate, mortgagePaymentMontly, lo
     return amortizationTable;
 }
 
+export function amortizationTableWithExtraPayment(interestRate, mortgagePaymentMontly, loanAmount, numberOfTerms, extraPaymentAnual, extraPaymentMontly) {
+    // the list will contain dictionaries with the structure:
+    // year, date, interest, principal, ending balance
+    let  endingBalance, term;
+    
+    // save it in another variable for making the code easier to read
+    endingBalance = loanAmount;
+
+    // initiate amortization table
+    let amortizationTable = [];
+
+    // console.log("interest interestRate",interestRate, "mortgagePaymentMontly", mortgagePaymentMontly, "loanAmount", loanAmount, "numberOfTerms", numberOfTerms, "extraPaymentAnual", extraPaymentAnual, "extraPaymentMontly", extraPaymentMontly);
+
+    // loop over the amortization table
+    for (let i = 0; i < numberOfTerms; i++) {
+        // assign value to the term
+        term = i+1;
+        
+        // so we will compute the interest and the principal for all the 12 months of a year
+        let currentMonthInterest = endingBalance*interestRate;
+        let currentMonthPrincipal = mortgagePaymentMontly - currentMonthInterest + extraPaymentMontly;
+
+        // substract normal principal payment to ending balance and extra monthly repayments
+        if (extraPaymentMontly <= endingBalance) {
+            // if monthly repayment is smaller than balance, add extra payment to principal an substract to the ending balance
+            currentMonthPrincipal += extraPaymentMontly;
+            endingBalance -= currentMonthPrincipal ;
+        } else {
+            // if monthly payment bigger than balance, only add the remaining to the principal payment for current month
+            currentMonthPrincipal += endingBalance - extraPaymentMontly ;
+            // ending balance is zero
+            endingBalance = 0;
+        }
+
+        // substract anual balance
+        if (term % 12 == 0 && extraPaymentAnual<= endingBalance) {
+            // if end of the year and the anual repayment is smaller than balance, add extra payment to principal an substract to the ending balance
+            currentMonthPrincipal += extraPaymentAnual;
+            endingBalance -=  extraPaymentAnual;
+        } else if (term % 12 == 0 && extraPaymentAnual > endingBalance) {
+            // if end of year and anual payment bigger than balance, only add the remaining to the principal payment for current month
+            currentMonthPrincipal += endingBalance - extraPaymentAnual;
+            // ending balance is zero
+            endingBalance = 0;
+        }
+
+        
+                
+        // add the values to the list
+        amortizationTable.push({
+            term: term,
+            interest: currentMonthInterest,
+            principal: currentMonthPrincipal,
+            endingBalance: endingBalance
+        });
+
+        // if already paid, break
+        if (endingBalance <= 0) {
+            break;
+        }
+
+        // advance date to the next month
+    }
+
+    // return value
+    return amortizationTable;
+}
+
 export function findRemainingDebtInAmortizationTableByTerm(remainingMonths, remainingYears, amortizationTable) {
     // returns the remaining balance based on the amortization table
 
@@ -71,3 +141,17 @@ export function findRemainingDebtInAmortizationTableByTerm(remainingMonths, rema
     return remainingBalance;
 
 }
+
+export function findCurrentPaymentIndexInAmortizationTable(remainingMonths, remainingYears, amortizationTable) {
+    // returns the remaining balance based on the amortization table
+
+    // using the remaining months and years, calculate the remaining payments
+    let remainingNumberOfPayments = remainingMonths + remainingYears*12;
+
+    // get the current index of the payment in the amortization table (indexes start with 0 on programming, so there is no need to add 1 to the result)
+    let currentIndexInAmortizationTable = amortizationTable.length - remainingNumberOfPayments;
+
+    // return value
+    return currentIndexInAmortizationTable;
+}
+
