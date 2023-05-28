8310ed10f70e7b25b6fce6a136bda6efa8135552
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-05-28 11:43:13 -0300

    added go to home link on 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,20 +1,20 @@
 <template>
     <div id="main-page-wrapper" class="container-sm my-5">
-      <h1 class="text-center my-5">Oops! The page you are looking for could not be found</h1>
-      <!-- Additional content or styling for the Not Found page -->
-      <picture class="d-flex align-items-center flex-column ">
-        <p>
+        <h1 class="text-center my-5">Oops! The page you are looking for could not be found</h1>
+        
+        <picture class="d-flex align-items-center flex-column ">
+            
 
-            <img src="../assets/imgs/not-found/flame-error-404.png" alt="Not found illustration"
-            class="mx-auto"
-            >
-        </p>
-        <!-- <div class="row"> -->
+                <img src="../assets/imgs/not-found/flame-error-404.png" alt="Not found illustration"
+                class="mx-auto"
+                >
+            
             <p class="text-center my-4">
                 Illustration by <a href="https://icons8.com/illustrations/author/oZpGJx8ts63Q" class="text-primary fw-semibold">Thierry Fousse</a> from <a href="https://icons8.com/illustrations" class="text-primary fw-semibold">Ouch!</a>
             </p>
-        <!-- </div> -->
-      </picture>
+        </picture>
+
+        <p class="text-center my-3">Go to <a href="/" class="text-primary fw-semibold">Home page</a></p>
     </div>
   </template>
   
