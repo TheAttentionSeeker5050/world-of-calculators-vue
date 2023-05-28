177df61056f4c49243a2f15751dcb2ccf50ed69f
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-05-28 11:39:37 -0300

    created error pages and edited a tag styles

--- src/components/globalComponents/Footer.vue
+++ src/components/globalComponents/Footer.vue
@@ -4,7 +4,7 @@
 <template>
 
 <footer class=" text-center fixed-bottom py-2 mt-4 footer bg-white" id="g-footer">
-    Site made by <a class="text-color-purple  text-primary" href="https://nicolas-castellano.com">
+    Site made by <a class="text-color-purple  text-primary fw-semibold" href="https://nicolas-castellano.com">
         Nicolas Castellano
     </a> 
 </footer>
