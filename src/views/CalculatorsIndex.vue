770e413568ff7cd03bfd5de76dd80b9dd07a0f60
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-05-29 18:05:10 -0300

    added more empty links to index

--- src/views/CalculatorsIndex.vue
+++ src/views/CalculatorsIndex.vue
@@ -28,16 +28,46 @@
 
         <h2>Math & Physics Calculators</h2>
         <ul>
-            <li><a class="fw-semibold text-primary" href="">Calculator 1 (pending)</a></li>
-            <li><a class="fw-semibold text-primary" href="">Calculator 2 (pending)</a></li>
-            <li><a class="fw-semibold text-primary" href="">Calculator 3 (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Fraction Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Random Number Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Hex to Binary Converter (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Binary to Hex Converter (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Quadratic Formula Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Root Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Greatest Common Factor Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Factor Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Rounding Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Matrix Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Triangle Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Square Volume Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Slope Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Circle Calculator (pending)</a></li>
+        </ul>
+
+        <h2>Statistic Calculators</h2>
+        <ul>
+            <li><a class="fw-semibold text-primary" href="">Standard Deviation Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Number Sequence Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Sample Size Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Mean, Median, Mode, Range Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">z-Score Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Permutation and Combination Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Confidence Interval Calculator (pending)</a></li>
         </ul>
 
         <h2>Fitness Calculators</h2>
         <ul>
-            <li><a class="fw-semibold text-primary" href="">Calculator 1 (pending)</a></li>
-            <li><a class="fw-semibold text-primary" href="">Calculator 2 (pending)</a></li>
-            <li><a class="fw-semibold text-primary" href="">Calculator 3 (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">BMI Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Calorie Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Calorie Calculator With Food Database Lookup (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Body Fat Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Ideal Weight Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Healthy Weight Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Macro Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Protein Intake Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Carb Intake Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Fat Intake Calculator (pending)</a></li>
+            <li><a class="fw-semibold text-primary" href="">Body Type Calculator (pending)</a></li>
         </ul>
 
         <br>
